25ce907f1dec70bae6fbf4c7b2869f0ba4006fea
Feli <maik@ehemanns.de>
2026-01-16 18:21:17 +0100

    Added some utility methods

--- src/main/java/de/feli490/hytale/privatechats/chat/Chat.java
+++ src/main/java/de/feli490/hytale/privatechats/chat/Chat.java
@@ -11,14 +11,16 @@ public class Chat {
 
     private final UUID id;
     private final ChatType chatType;
+    private final long created;
 
     private final Set<PlayerChatRole> playerChatRoles;
     private final List<ChatMessage> messages;
     private final List<ChatMessage> unmodifiableMessageList;
 
-    private Chat(UUID id, ChatType chatType) {
+    private Chat(UUID id, ChatType chatType, long created) {
         this.id = id;
         this.chatType = chatType;
+        this.created = created;
 
         playerChatRoles = new HashSet<>();
         messages = new ArrayList<>();
@@ -30,7 +32,7 @@ public class Chat {
     }
 
     public void sendMessage(UUID senderId, String message) {
-        messages.add(new ChatMessage(UUID.randomUUID(), senderId, message));
+        messages.add(new ChatMessage(UUID.randomUUID(), senderId, message, System.currentTimeMillis()));
     }
 
     public void removeChatter(UUID playerId) {
@@ -38,20 +40,58 @@ public class Chat {
                                                                  .equals(playerId));
     }
 
+    public ChatRole getRole(UUID playerId) {
+        return playerChatRoles.stream()
+                              .filter(playerChatRole -> playerChatRole.getPlayerId()
+                                                                      .equals(playerId))
+                              .findFirst()
+                              .map(PlayerChatRole::getRole)
+                              .orElse(null);
+    }
+
+    public long getCreated() {
+        return created;
+    }
+
+    public long getLastUpdate() {
+
+        ChatMessage lastMessage = getLastMessage();
+        if (lastMessage != null)
+            return lastMessage.timestamp();
+
+        return created;
+    }
+
+    public ChatMessage getLastMessage() {
+        if (messages.isEmpty())
+            return null;
+        return messages.getLast();
+    }
+
+    public boolean isMember(UUID uuid) {
+        return playerChatRoles.stream()
+                              .anyMatch(playerChatRole -> playerChatRole.getPlayerId()
+                                                                        .equals(uuid));
+    }
+
     public List<ChatMessage> getMessages() {
         return unmodifiableMessageList;
     }
 
     public static Chat createDirect(UUID player1, UUID player2) {
-        Chat chat = new Chat(UUID.randomUUID(), ChatType.DIRECT);
+        Chat chat = createChat(ChatType.DIRECT);
         chat.addChatter(player1, ChatRole.OWNER);
         chat.addChatter(player2, ChatRole.OWNER);
         return chat;
     }
 
     public static Chat createGroup(UUID player1) {
-        Chat chat = new Chat(UUID.randomUUID(), ChatType.GROUP);
+        Chat chat = createChat(ChatType.GROUP);
         chat.addChatter(player1, ChatRole.OWNER);
         return chat;
+    }
+
+    private static Chat createChat(ChatType chatType) {
+        return new Chat(UUID.randomUUID(), chatType, System.currentTimeMillis());
     }
 }
--- src/main/java/de/feli490/hytale/privatechats/chat/ChatMessage.java
+++ src/main/java/de/feli490/hytale/privatechats/chat/ChatMessage.java
@@ -2,6 +2,6 @@ package de.feli490.hytale.privatechats.chat;
 
 import java.util.UUID;
 
-public record ChatMessage(UUID id, UUID senderId, String message) {
+public record ChatMessage(UUID id, UUID senderId, String message, Long timestamp) {
 
 }
